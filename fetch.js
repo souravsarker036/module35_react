8d8e2749fa2e6b72ab95dee69447cb563cc9c16d
Sourav Sarker <souravsarker036@gmail.com>
2025-10-15 22:23:30 +0600

    fetch

---
+++ fetch.js
@@ -0,0 +1,24 @@
+const person ={
+    name:"Hero alom",
+    age: 40,
+    friends: ["bd","CN","rahim"],
+    family:{
+        fatherName:"karim",
+        motherName:"jorina"
+    }
+}
+
+// const jsonData= JSON.stringify(person);
+// const planData = JSON.parse(jsonData)
+// console.log(planData)
+
+// // fetch
+
+// fetch("url")
+// .then(res>res.json())
+// .then(data=>console.log(""))
+
+// const keys = Object.keys(person)
+// const values = Object.values(person)
+const newArray = [...person,]
+console.log(values)
